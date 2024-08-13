cd223e7625ac304e0954e6b7a235a2aacb5c1f57
HienDao <hientto1234@gmail.com>
2024-08-13 21:54:23 +0700

    Update something :D

--- app/src/main/java/hiendao/moviefinder/util/shared_components/ImagesScreen.kt
+++ app/src/main/java/hiendao/moviefinder/util/shared_components/ImagesScreen.kt
@@ -1,26 +1,22 @@
 package hiendao.moviefinder.util.shared_components
 
-
 import android.Manifest
 import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Build
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.gestures.detectHorizontalDragGestures
-import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PageSize
+import androidx.compose.foundation.pager.PagerDefaults
+import androidx.compose.foundation.pager.PagerSnapDistance
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Download
@@ -28,31 +24,18 @@ import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
-import androidx.compose.ui.window.DialogProperties
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.toBitmap
-import coil.compose.AsyncImage
 import coil.compose.AsyncImagePainter
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
@@ -62,20 +45,29 @@ import hiendao.moviefinder.util.saveImageToExternalStorage
 import java.util.UUID
 
 @Composable
-fun ImageScreen(
-    uri: String,
+fun ImagesScreen(
     modifier: Modifier = Modifier,
-    title: String,
-    setShowImage: (Boolean) -> Unit
+    images: List<String>,
+    setShowImage: (Boolean) -> Unit,
+    isPoster: Boolean
 ) {
-    val context = LocalContext.current
-    var scale by remember {
-        mutableStateOf(1f)
-    }
-    var offset by remember {
-        mutableStateOf(Offset(0f, 0f))
+    val pagerState = rememberPagerState(
+        initialPage = 0,
+        initialPageOffsetFraction = 0f
+    ) {
+        images.size
     }
 
+    var imageIndex by remember {
+        mutableIntStateOf(0)
+    }
+
+    val context = LocalContext.current
+
+    val fling = PagerDefaults.flingBehavior(
+        state = pagerState,
+        pagerSnapDistance = PagerSnapDistance.atMost(images.size)
+    )
     val requestPermission = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.RequestPermission(),
         onResult = { isGranted ->
@@ -85,7 +77,7 @@ fun ImageScreen(
 
     val imagePainter = rememberAsyncImagePainter(
         model = ImageRequest.Builder(context)
-            .data(makeFullUrl(uri))
+            .data(makeFullUrl(images[imageIndex]))
             .size(Size.ORIGINAL)
             .build()
     )
@@ -94,15 +86,15 @@ fun ImageScreen(
 
     Dialog(
         onDismissRequest = { setShowImage(false) },
-        properties = DialogProperties(
-            dismissOnBackPress = true,
-            dismissOnClickOutside = true,
-            usePlatformDefaultWidth = false
-        )
+//        properties = DialogProperties(
+//            dismissOnBackPress = true,
+//            dismissOnClickOutside = true,
+//            usePlatformDefaultWidth = false
+//        )
     ) {
         Column(
             modifier = modifier
-                .fillMaxSize()
+                .fillMaxWidth()
                 .background(MaterialTheme.colorScheme.background)
                 .padding(bottom = 10.dp, start = 10.dp, end = 10.dp),
             verticalArrangement = Arrangement.SpaceBetween
@@ -116,13 +108,14 @@ fun ImageScreen(
                 ) {
                     Icon(
                         imageVector = Icons.Default.Close,
-                        contentDescription = "Thoát"
+                        contentDescription = "Close"
                     )
                 }
 
                 Row(horizontalArrangement = Arrangement.spacedBy(10.dp)) {
                     IconButton(
                         onClick = {
+
                             var hasWriteExternalStorage = ContextCompat.checkSelfPermission(
                                 context,
                                 Manifest.permission.WRITE_EXTERNAL_STORAGE
@@ -161,7 +154,7 @@ fun ImageScreen(
                     ) {
                         Icon(
                             imageVector = Icons.Default.Download,
-                            contentDescription = "Thoát"
+                            contentDescription = "Download"
                         )
                     }
                     IconButton(
@@ -171,41 +164,42 @@ fun ImageScreen(
                     ) {
                         Icon(
                             imageVector = Icons.Default.Share,
-                            contentDescription = "Thoát"
+                            contentDescription = "Share"
                         )
                     }
                 }
             }
 
-            AsyncImage(
-                model = makeFullUrl(uri),
-                contentDescription = title,
-                contentScale = ContentScale.Fit,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .clip(RoundedCornerShape(10.dp))
-                    .pointerInput(Unit) {
-                        detectTransformGestures { _, pan, zoom, _ ->
-                            scale *= zoom
-                            scale = scale.coerceIn(0.5f, 3f)
-                            offset = if (scale == 1f) Offset(0f, 0f) else offset + pan
+            HorizontalPager(
+                modifier = Modifier.fillMaxWidth().align(Alignment.CenterHorizontally),
+                state = pagerState,
+                flingBehavior = fling,
+                key = {
+                    images[it]
+                },
+                pageSize = PageSize.Fill
+            ) {index ->
+                imageIndex = index
+                if(isPoster){
+                    CustomImage(
+                        imageUrl = images[index],
+                        width = 300.dp,
+                        height = 500.dp,
+                        onClick = {
+
                         }
-
-                    }
-                    .graphicsLayer(
-                        scaleX = scale, scaleY = scale,
-                        translationX = offset.x, translationY = offset.y
                     )
+                } else {
+                    CustomImage(
+                        imageUrl = images[index],
+                        width = 350.dp,
+                        height = 200.dp,
+                        onClick = {
 
-            )
-            Text(
-                text = title,
-                fontSize = 16.sp,
-                fontStyle = FontStyle.Italic,
-                modifier = Modifier.fillMaxWidth(),
-                textAlign = TextAlign.Center
-            )
-
+                        }
+                    )
+                }
+            }
         }
     }
 }
